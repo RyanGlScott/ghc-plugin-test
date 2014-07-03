6cc5cdec2ad89f9769d468280f6d41fffd68bef2
RyanGlScott <ryan.gl.scott@ku.edu>
2014-07-03 14:06:37 -0500

    Stuff and things

---
+++ src/TestPlugin.hs
@@ -0,0 +1,14 @@
+module TestPlugin (plugin) where
+
+import GhcPlugins
+
+plugin :: Plugin
+plugin = defaultPlugin {
+  installCoreToDos = install
+}
+
+install :: [CommandLineOption] -> [CoreToDo] -> CoreM [CoreToDo]
+install _ todo = do
+  reinitializeGlobals
+  putMsgS "Hello!"
+  return todo
